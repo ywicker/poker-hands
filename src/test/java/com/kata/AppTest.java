505c8edf5a2aebe195e6bab95bf4c5b69769c124
ywicker <yoann.wicker@gmail.com>
2023-04-28 13:06:22 +0200

    init

---
+++ src/test/java/com/kata/AppTest.java
@@ -0,0 +1,12 @@
+package com.kata;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void test(){
+        Assertions.assertThat(true).isTrue();
+    }
+}
