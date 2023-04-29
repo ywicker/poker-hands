481d8fc71f329665d39ceb26ce04f48644fe81b5
ywicker <yoann.wicker@gmail.com>
2023-04-29 15:11:49 +0200

    Pair : find pair from poker hand

--- src/main/java/com/kata/PokerHand.java
+++ src/main/java/com/kata/PokerHand.java
@@ -1,10 +1,22 @@
 package com.kata;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record PokerHand(Card... cards) {
     public CardValue maxCardValue() {
         return Arrays.stream(cards).max(Card::compareTo)
                 .orElseThrow().value();
+    }
+
+    public Set<Pair> pairs() {
+        Map<CardValue, List<Card>> groupByValues = Arrays.stream(cards).collect(Collectors.groupingBy(Card::value));
+        return groupByValues.entrySet().stream().filter(cardValueListEntry -> cardValueListEntry.getValue().stream().count() == 2)
+                .map(Map.Entry::getKey)
+                .map(Pair::new)
+                .collect(Collectors.toSet());
     }
 }
---
+++ src/test/java/com/kata/PokerHandTest.java
@@ -0,0 +1,49 @@
+package com.kata;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static com.kata.CardValue.AS;
+import static com.kata.CardValue.FOUR;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PokerHandTest {
+
+    @Nested
+    class Pairs {
+        @Test
+        void should_return_pair() {
+            // given
+            var pokerHand = new PokerHand(new Card(FOUR), new Card(FOUR));
+
+            // when
+            var pairs = pokerHand.pairs();
+
+            // then
+            assertThat(pairs).containsExactly(new Pair(FOUR));
+        }
+        @Test
+        void should_not_return_pair_when_have_not_pairs() {
+            // given
+            var pokerHand = new PokerHand(new Card(AS), new Card(FOUR));
+
+            // when
+            var pairs = pokerHand.pairs();
+
+            // then
+            assertThat(pairs).isEmpty();
+        }
+        @Test
+        void should_not_return_pair_when_have_three_of_kind() {
+            // given
+            var pokerHand = new PokerHand(new Card(FOUR), new Card(FOUR), new Card(FOUR));
+
+            // when
+            var pairs = pokerHand.pairs();
+
+            // then
+            assertThat(pairs).isEmpty();
+        }
+
+    }
+}
