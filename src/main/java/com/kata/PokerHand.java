922e5fde4157b99134c67efbc9f915e580640197
ywicker <yoann.wicker@gmail.com>
2023-04-29 12:29:00 +0200

    refactor HighCardWins : add maxCardValue

--- src/main/java/com/kata/PokerHand.java
+++ src/main/java/com/kata/PokerHand.java
@@ -1,4 +1,10 @@
 package com.kata;
 
+import java.util.Arrays;
+
 public record PokerHand(Card... cards) {
+    public CardValue maxCardValue() {
+        return Arrays.stream(cards).max(Card::compareTo)
+                .orElseThrow().value();
+    }
 }
--- src/main/java/com/kata/PokerRound.java
+++ src/main/java/com/kata/PokerRound.java
@@ -1,20 +1,16 @@
 package com.kata;
 
-import java.util.Arrays;
-
 import static com.kata.PokerResult.*;
 
 public record PokerRound(PokerHand blackPokerHand, PokerHand whitePokerHand) {
     public PokerResult result() {
-        var maxValueBlackPokerHand = Arrays.stream(blackPokerHand.cards())
-                .max(Card::compareTo).get();
-        var maxValueWhitePokerHand = Arrays.stream(whitePokerHand.cards())
-                .max(Card::compareTo).get();
+        var maxValueBlackPokerHand = blackPokerHand.maxCardValue();
+        var maxValueWhitePokerHand = whitePokerHand.maxCardValue();
 
-        switch (maxValueBlackPokerHand.compareTo(maxValueWhitePokerHand)) {
-            case 1: return BLACK_WINS;
-            case -1: return WHITE_WINS;
-            default: return EGALITY;
-        }
+        return switch (maxValueBlackPokerHand.compareTo(maxValueWhitePokerHand)) {
+            case 1 -> BLACK_WINS;
+            case -1 -> WHITE_WINS;
+            default -> EGALITY;
+        };
     }
 }
